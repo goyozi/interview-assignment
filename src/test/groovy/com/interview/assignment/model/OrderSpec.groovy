1a8ead3df31a1e7a0370bc5942c7bbce26e27dd5
Grzegorz Ziemonski <grzegorz.ziemonski@gmail.com>
2018-12-08 19:54:42 +0100

    Express the independence of product/item prices in the OrderSpec

--- src/test/groovy/com/interview/assignment/model/OrderSpec.groovy
+++ src/test/groovy/com/interview/assignment/model/OrderSpec.groovy
@@ -7,12 +7,19 @@ class OrderSpec extends Specification {
     static final product2 = new Product("doesn't matter", 2.34)
 
     def "total amount"() {
-        given:
+        given: "an order with two items"
         def item1 = new OrderItem(product1, 2)
         def item2 = new OrderItem(product2, 3)
         def order = new Order("doesnt@matter.com", [item1, item2])
 
-        expect:
+        expect: "total amount to be calculated correctly"
+        order.totalAmount() == 9.48
+
+        when: "the price of the ordered products changes"
+        product1.price = 3.45
+        product2.price = 4.56
+
+        then: "total amount remains the same"
         order.totalAmount() == 9.48
     }
 }
